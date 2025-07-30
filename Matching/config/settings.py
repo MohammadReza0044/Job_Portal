3d0b0a738310c4440e521c83559a50e2aecb301c
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2025-07-30 22:55:25 +0100

    list(config("ALLOWED_HOSTS").split(",")) has been added to all services setting

--- Matching/config/settings.py
+++ Matching/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = list(config("ALLOWED_HOSTS").split(","))
 
 
 # Application definition
