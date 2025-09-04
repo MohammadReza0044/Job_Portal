cfe02838a42b4b1941c919ba06c1058fa0646b12
MohammadReza0044 <mohammadreza.shayegan@gmail.com>
2025-09-04 14:10:01 +0100

    job unit test updated. unauthenticate user has been added.

--- Job/job/tests.py
+++ Job/job/tests.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from job.models import Category, Job, Location
+from utils.generate_jwt import generate_test_jwt
 
 
 class AuthenticatedJobViewsTests(TestCase):
@@ -18,6 +19,8 @@ class AuthenticatedJobViewsTests(TestCase):
 
         # Authorized user
         self.user_id = uuid.uuid4()
+        self.token = generate_test_jwt(user_id=self.user_id, role="Employer")
+        self.auth_headers = {"HTTP_AUTHORIZATION": f"Bearer {self.token}"}
 
         # Jobs
         self.job1 = Job.objects.create(
@@ -41,12 +44,22 @@ class AuthenticatedJobViewsTests(TestCase):
 
     def test_get_all_jobs(self):
         url = reverse("job:job_list")
-        response = self.client.get(url)
+        response = self.client.get(url, **self.auth_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["Result"]), 2)
 
     def test_get_single_job(self):
         url = reverse("job:job_detail", kwargs={"job_id": str(self.job1.id)})
-        response = self.client.get(url)
+        response = self.client.get(url, **self.auth_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["Result"]["title"], "Backend Developer")
+
+    def test_get_jobs_unauthenticated(self):
+        url = reverse("job:job_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_single_job_unauthenticated(self):
+        url = reverse("job:job_detail", kwargs={"job_id": str(self.job1.id)})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
